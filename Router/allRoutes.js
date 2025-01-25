4570ab0c53ac418d5779914983b60ffaaf0460d4
guenaellouer <guenael.louer@gmail.com>
2025-01-25 23:15:55 +0100

    Installation des modules de connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Insciption", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre affiche comme ceci : Route.titre - websitename
